71d19fe740cca20178a07daeeceaa0137523bdb0
Claire Alvis <claire.alvis@gmail.com>
2015-04-29 15:39:02 -0400

    v0.4.4

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject spec-tacular "0.4.3"
+(defproject spec-tacular "0.4.4"
   :description "First-class, extendable data specifications for clojure."
   :url "https://github.com/SparkFund/spec-tacular"
   :license {:name "Apache License, Version 2.0"
