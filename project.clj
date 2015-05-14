b46df3a5f90c3d746d44139ec2e45a1ac592dd14
Nathan Sorenson <nathan.d.sorenson@gmail.com>
2015-05-13 17:40:32 -0700

    v0.4.7

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject spec-tacular "0.4.6"
+(defproject spec-tacular "0.4.7"
   :description "First-class, extendable data specifications for clojure."
   :url "https://github.com/SparkFund/spec-tacular"
   :license {:name "Apache License, Version 2.0"
